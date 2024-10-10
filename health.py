534b9e7d2891afdc822855793193f996b2710c3b
Albert Cintas <167300436+AlbertCintas-insurteam@users.noreply.github.com>
2024-10-10 17:07:26 +0200

    Create health.py

---
+++ health.py
@@ -0,0 +1,29 @@
+# This HealthCheck is used by ECS to make sure the container is running
+
+import logging
+
+import requests
+
+# Logging to stout
+logging.basicConfig(filename="/proc/1/fd/1",
+                    format="{'time':'%(asctime)s', 'level': '%(levelname)s', \
+                           'message': '%(message)s'}",
+                    level=logging.INFO)
+
+URL = 'http://127.0.0.1'
+PORT = '8000'
+TIMEOUT = 5
+
+logging.info("Starting Health Check to %s:%s", URL, PORT)
+
+# Health Check
+response = requests.get(URL + ':' + PORT + '/health/?format=json', timeout=TIMEOUT)
+
+if response.status_code == requests.codes.ok:
+    logging.info(
+        f'Health check {URL}: HTTP status code {response.status_code}')
+    exit(0)
+else:
+    logging.error(
+        f'Health check {URL}: HTTP status code {response.status_code}')
+    exit(1)
